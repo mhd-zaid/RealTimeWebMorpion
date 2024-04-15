0078bac1eb0d7fb5446065e1f1c403acbfcb1fa2
dan1M <maneadaniel555@gmail.com>
2024-04-15 11:46:56 +0200

    basic config front

--- client/src/components/Tasks/TaskForm.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TaskForm({ onSubmit, defaultValues }) {
-  return <div>TaskForm</div>;
-}
-
-export default TaskForm;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import extend_theme from './utils/chakra-theme.js';
+import App from './App.jsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const theme = extendTheme(extend_theme);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<App />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
