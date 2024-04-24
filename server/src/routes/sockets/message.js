a1f58b60ea7dad7cd0391eab230bb587e52143de
dan1M <maneadaniel555@gmail.com>
2024-04-24 21:16:14 +0200

    update prettier server

--- server/src/routes/sockets/message.js
+++ server/src/routes/sockets/message.js
@@ -1,34 +1,35 @@
-export default (io,db) => {
-  io.of("/messages").on("connection",socket => {
-    socket.emit("messages:list", async () => { return { status: "success", data: await db.Message.findAll()}});
+export default (io, db) => {
+  io.of('/messages').on('connection', socket => {
+    socket.emit('messages:list', async () => {
+      return { status: 'success', data: await db.Message.findAll() };
+    });
 
-    socket.emit("messages:create", async () => {
+    socket.emit('messages:create', async () => {
       await db.Message.create({
         content: data.content,
         userId: data.userId,
         roomId: null,
       });
-      return {status: "success", message: "messages created"}
+      return { status: 'success', message: 'messages created' };
     });
 
-    socket.emit("messages:create:room", async (data) => {
+    socket.emit('messages:create:room', async data => {
       await db.Message.create({
         content: data.content,
         userId: data.userId,
         roomId: data.roomId,
       });
-      return {status: "success", message: "messages created"}
+      return { status: 'success', message: 'messages created' };
     });
-    
-    socket.emit("messages:list:room", async (data) => {
+
+    socket.emit('messages:list:room', async data => {
       await db.Message.findAll({
         where: {
           roomId: data.roomId,
-        }
+        },
       });
-      return {status: "success", data: messages}
+      return { status: 'success', data: messages };
     });
-    
   });
 
   return io;
