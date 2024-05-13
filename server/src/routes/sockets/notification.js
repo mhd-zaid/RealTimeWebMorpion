14a3c1219d4b9df0ba4ec420c30679180b0889b7
mhd-zaid <zaidmouhamad@gmail.com>
2024-05-13 15:18:18 +0200

    middleware isadmin for notifiction

--- server/src/routes/sockets/notification.js
+++ server/src/routes/sockets/notification.js
@@ -1,5 +1,18 @@
+import checkAuthSocket from '../../middlewares/checkAuthSocket.js';
+
 export default io => {
-  io.on('connection', async socket => {
+  io.use(async (socket, next) => {
+    const checkUser = await checkAuthSocket(socket, db);
+    if (checkUser === false) {
+      return next(new Error('Vous devez être connecté'));
+    }
+    const checkAdmin = checkUser.role === 'admin';
+    if (checkAdmin === false) {
+      return next(new Error('Vous devez être admin'));
+    }
+    socket.user = checkUser;
+    next();
+  }).on('connection', async socket => {
     socket.on('send:notification', message => {
       socket.broadcast.emit('notification', message);
     });
